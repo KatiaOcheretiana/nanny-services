3ca6e3913abadae42893de47173b51168ab151bf
KatiaOcheretiana <130676842+KatiaOcheretiana@users.noreply.github.com>
2024-04-24 18:45:07 +0300

    initial commit

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -1,9 +1,5 @@
 import styled, { createGlobalStyle } from "styled-components";
 import "modern-normalize";
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
 
 export const GlobalStyle = createGlobalStyle`
 html {
