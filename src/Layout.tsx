c486f487f7b9f8c69faa2242b824d811f9253145
KatiaOcheretiana <130676842+KatiaOcheretiana@users.noreply.github.com>
2024-04-30 19:47:09 +0300

    clean code to fix bug

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,14 +1,11 @@
 import { Suspense } from "react";
 import { Toaster } from "react-hot-toast";
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import { Navigation } from "./components/Navigation/Navigation";
 
 export const Layout = () => {
-  // const location = useLocation();
-
   return (
     <>
-      {/* {location.pathname === "/" && */}
       <Navigation />
 
       <Suspense fallback={<p>loading///</p>}>
