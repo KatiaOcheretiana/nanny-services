1a43f674bd1478e3f631c569b0d7101170ddc158
KatiaOcheretiana <130676842+KatiaOcheretiana@users.noreply.github.com>
2024-05-13 19:11:22 +0000

    Deploying to gh-pages from @ KatiaOcheretiana/nanny-services@d192850a1fe137f22ee05741acf11719c7356b50 🚀

---
+++ static/js/782.ff4316e6.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunknanny_services=self.webpackChunknanny_services||[]).push([[782],{782:(e,s,n)=>{n.r(s),n.d(s,{default:()=>l});var r=n(3003),t=n(5482),a=n(2566),c=n(6412),d=n(3163),i=n(579);const l=()=>{const e=(0,r.d4)(t.H),s=(0,r.d4)(a.dd).filter((s=>e.includes(s.name)));return(0,i.jsx)(c.m,{children:s.length>0?(0,i.jsx)(d.G,{data:s}):(0,i.jsx)("p",{children:"Your favorite list is empty."})})}}}]);
+//# sourceMappingURL=782.ff4316e6.chunk.js.map
